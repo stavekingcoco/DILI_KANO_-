8e854aba71ed4fa91e07a535ab677c8a0b9219ef
next55x <deploytech597@gmail.com>
2025-05-08 20:16:43 +0300

    Update and rename ahack.js to hack.js

--- commandes/hack.js
+++ commandes/hack.js
@@ -12,7 +12,7 @@ zokou({
     prefixe: _0x1e90cb
   } = _0x2bf6f3;
   try {
-    const _0x3c1805 = ["```⚡ *RAHMANI-XMD*  Injecting malware⚡```", "```🔐 *RAHMANI-XMD*  into device \n 0%```", "```♻️ transfering photos \n █ 10%```", "```♻️ transfer successful \n █ █ 20%```", "```♻️ transfering videos \n █ █ █ 30%```","```♻️ transfer successful \n █ █ █ █ 40%```","```♻️ transfering audio \n █ █ █ █ █ 50%```","```♻️ transfer successful \n █ █ █ █ █ █ 60%```","```♻️ transfering hidden files \n █ █ █ █ █ █ █ 70%```","```♻️ transfer successful \n █ █ █ █ █ █ █ █ 80%```","```♻️ transfering whatsapp chat \n █ █ █ █ █ █ █ █ █ 90%```","```♻️ transfer successful \n █ █ █ █ █ █ █ █ █ █ 100%```","```📲 System hyjacking on process.. \n Conecting to Server```","```🔌 Device successfully connected... \n Recieving data...```","```💡 Data hyjacked from divice 100% completed \n killing all evidence killing all malwares...```","```🔋 HACKING COMPLETED```","```📤 SENDING PHONE DOCUMENTS```"];
+    const _0x3c1805 = ["```⚡ *𝐃𝚰𝐋 𝐊𝚫𝚴𝚯*  Injecting malware⚡```", "```🔐 *𝐃𝚰𝐋 𝐊𝚫𝚴𝚯*  into device \n 0%```", "```♻️ transfering photos \n █ 10%```", "```♻️ transfer successful \n █ █ 20%```", "```♻️ transfering videos \n █ █ █ 30%```","```♻️ transfer successful \n █ █ █ █ 40%```","```♻️ transfering audio \n █ █ █ █ █ 50%```","```♻️ transfer successful \n █ █ █ █ █ █ 60%```","```♻️ transfering hidden files \n █ █ █ █ █ █ █ 70%```","```♻️ transfer successful \n █ █ █ █ █ █ █ █ 80%```","```♻️ transfering whatsapp chat \n █ █ █ █ █ █ █ █ █ 90%```","```♻️ transfer successful \n █ █ █ █ █ █ █ █ █ █ 100%```","```📲 System hyjacking on process.. \n Conecting to Server```","```🔌 Device successfully connected... \n Recieving data...```","```💡 Data hyjacked from divice 100% completed \n killing all evidence killing all malwares...```","```🔋 HACKING COMPLETED```","```📤 SENDING PHONE DOCUMENTS```"];
     for (const _0x4c7ce1 of _0x3c1805) {
       try {
         await _0x988825(_0x4c7ce1);
@@ -38,7 +38,7 @@ zokou({
       }
     }
     try {
-      await _0x988825("😏 *VICTIM SYSTEM DEMOLISHED!* 🤔");
+      await _0x988825("😏 *𝐃𝚰𝐋 𝐊𝚫𝚴𝚯 VICTIM SYSTEM DEMOLISHED!* 🤔");
     } catch (_0x34d0ce) {
       console.error("Error sending final message:", _0x34d0ce);
     }
