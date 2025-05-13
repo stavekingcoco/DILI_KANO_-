381a4f58fae20d1143572ba673a016c28cc64e33
next55x <deploytech597@gmail.com>
2025-05-13 19:34:19 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 𝐃𝚰𝐋 𝐊𝚫𝚴𝚯\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('𝐃𝚰𝐋 𝐊𝚫𝚴𝚯', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy dilikano\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('dilikano', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
